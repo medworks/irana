1dbeae7d9893e80d64f335706c4ea797809971b4
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2014-08-02 12:37:12 +0330

    add homepage and other files

---
+++ index.php
@@ -0,0 +1,197 @@
+<!doctype html>
+
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+
+<html lang="en" class="no-js"><!--<![endif]--><head>
+	<meta charset="utf-8">
+	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
+	   Remove this if you use the .htaccess -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title></title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<!--  Mobile viewport optimized: j.mp/bplateviewport -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Place favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+	<link rel="shortcut icon" href="/favicon.ico">
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+	<!-- CSS : implied media="all" -->
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/rainbow.css" type="text/css">
+</head>
+<body>
+  	<div id="container">
+		<!-- Header -->
+	    <div id="header">
+			<div id="logo">
+				<a href="index.html">
+					<img src="images/logo.png" alt="">
+				</a>
+			</div>
+			<div id="personal_data">
+				<p>Phone <span>+42 555-46-65</span></p>
+				<p>email: <span><a href="mailto:johndoe@johndoes.com">johndoe@johndoes.com</a></span></p>
+			</div>
+		</div>
+		<div id="menu" class="png_bg">
+			<ul>
+				<li class="selected">
+					<span class="menu_box_left"></span>
+					<a href="index.html">Home</a>
+					<span class="menu_box_right"></span>
+				</li>
+				<li>
+					<span class="menu_box_left"></span>
+					<a href="#" class="">Features</a>
+					<ul style="display: none;">
+						<li class="first_sub png_bg">
+							<a href="typography.html">Typography<span> »</span></a>
+							<ul style="display: none;">
+								<li class="first_sub png_bg"><a href="typography.html">Headings and paragraphs</a></li>
+								<li><a href="floats.html">Images and floats</a></li>
+								<li><a href="dropcaps.html">Dropcaps and blockquotes</a></li>
+								<li><a href="checklists.html">Code blocks and checklists</a></li>
+								<li><a href="buttons.html">Buttons</a></li>
+								<li><a href="boxes.html">Warning boxes</a></li>
+								<li><a href="separators.html">Separators</a></li>
+								<li><a href="tables.html">Tables</a></li>
+								<li><a href="showcase.html">Grids and container blocks</a></li>
+								<li><a href="pricing.html">Pricing tables</a></li>
+								<li><a href="toggle.html">FAQ toggles</a></li>
+								<li class="last_sub png_bg"></li>
+							</ul>
+						</li>
+						<li><a href="showcase.html">Grids showcase</a></li>
+						<li><a href="pricing.html">Pricing tables</a></li>
+						<li><a href="about.html">About</a></li>
+						<li class="last_sub png_bg"></li>
+					</ul>
+					<span class="menu_box_right"></span>
+				</li>
+				<li>
+					<span class="menu_box_left"></span>
+					<a href="#" class="">Pages</a>
+					<ul style="display: none;">
+						<li class="first_sub png_bg">
+							<a href="#">Mainpage styles<span> »</span></a>
+							<ul style="display: none;">
+								<li class="first_sub png_bg"><a href="index.html">Default</a></li>
+								<li><a href="mainpage_pricing.html">With pricing</a></li>
+								<li><a href="mainpage_pricing_nowidg.html">With pricing, without widgets</a></li>
+								<li><a href="mainpage_news.html">With news section</a></li>
+								<li><a href="mainpage_noslogan.html">Without slogan area</a></li>
+								<li><a href="mainpage_noslider.html">Without slider</a></li>
+								<li><a href="mainpage_vid.html">With video</a></li>
+								<li class="last_sub png_bg"></li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">Portfolio styles<span> »</span></a>
+							<ul style="display: none;">
+								<li class="first_sub png_bg"><a href="portfolio4c.html">4-columns</a></li>
+								<li><a href="portfolio.html">Default, 3-columns</a></li>
+								<li><a href="portfolio2c.html">2-columns</a></li>
+								<li><a href="portfolio1c.html">1-column</a></li>
+								<li><a href="portfolio_allinone.html">All in one</a></li>
+								<li class="last_sub png_bg"></li>
+							</ul>
+						</li>
+						<li><a href="#">Blog styles<span> »</span></a>
+							<ul style="display: none;">
+								<li class="first_sub png_bg"><a href="blog.html">Default</a></li>
+								<li><a href="blog2.html">Sidebar left</a></li>
+								<li><a href="blog3.html">Double sidebars</a></li>
+								<li class="last_sub png_bg"></li>
+							</ul>
+						</li>
+						<li><a href="#">Contact styles<span> »</span></a>
+							<ul style="display: none;">
+								<li class="first_sub png_bg"><a href="contact.html">Default</a></li>
+								<li><a href="contact2.html">Style no.2</a></li>
+								<li><a href="contact3.html">Style no.3</a></li>
+								<li class="last_sub png_bg"></li>
+							</ul>
+						</li>
+						<li><a href="404.html">Error 404 page</a></li>
+						<li class="last_sub png_bg"></li>
+					</ul>
+					<span class="menu_box_right"></span>
+				</li>
+				<li>
+					<span class="menu_box_left"></span>
+					<a href="blog.html" class="">Blog</a>
+					<span class="menu_box_right"></span>
+				</li>
+				<li>
+					<span class="menu_box_left"></span>
+					<a href="portfolio.html">Portfolio</a>
+					<span class="menu_box_right"></span>
+				</li>
+				<li>
+					<span class="menu_box_left"></span>
+					<a href="contact.html" class="">Contact</a>
+					<span class="menu_box_right"></span>
+				</li>
+			</ul>
+		</div>
+    	<!-- /Header -->
+		<!-- Main content alpha -->
+		<div class="main png_bg">
+			<div class="inner_main">
+			<!-- True containers (keep the content inside containers!) -->
+				<div class="container_alpha slider">
+					<div id="slider" class="nivoSlider" style="display: block; position: relative; height: 350px; background: url(http://etherna.html.themeforest.designsentry.com/images/slides/slide5.jpg) no-repeat;">
+						<img src="images/slides/slide1.jpg" alt="" title="#htmlcaption" style="display: none;">
+						<img src="images/slides/slide2.jpg" alt="" style="display: none;">
+						<img src="images/slides/slide3.jpg" alt="" title="#htmlcaption2" style="display: none;">
+						<img src="images/slides/slide4.jpg" alt="" style="display: none;">
+						<img src="images/slides/slide5.jpg" alt="" style="display: none;">
+						<img src="images/slides/slide6.jpg" alt="" style="display: none;">
+					</div>
+					<div id="htmlcaption" class="nivo-html-caption">
+						<h3>Hello!</h3>
+						<p>Welcome to the Etherna, a truly flexible and beautiful theme that will be a great base for your new portfolio, company site or blog. </p>
+						<p>Take a look at the available backgrounds and color presets that I've prepared, there are 27 variations, and each one of them is great!</p>
+					</div>
+					<div id="htmlcaption2" class="nivo-html-caption">
+						<h3>Etherna is..</h3>
+						<p>..something different. The details and precision in styling is a result of over 200 hours of hard work, and you pay for half of one!</p>
+						<p>Now this is what I call a bargain! </p>
+						<p><a href="typography.html" class="superbutton">+ See more..</a></p>
+					</div>
+					<div class="loader" style="display: none;"></div>
+				</div>
+				<div class="container_gamma slogan">
+					<img src="images/info_button.png" class="alignright" alt="">
+					<h1>This is space that I call 
+						<strong>slogan area.</strong> It's purpose is to drive your 
+						<strong>att- <br>ention</strong> to something important, like button that will say 
+						<strong>"buy me!"</strong>
+					</h1>
+				</div>
+			</div>
+			<!-- /True containers (keep the content inside containers!) -->
+    	</div>
+    	<div class="endmain png_bg"></div>
+		<!-- /Main content alpha -->
+	</div>
+  	<!--! end of #container -->
+  	<!-- Javascript at the bottom for fast page loading -->
+  	<!-- Grab Google CDN's jQuery. fall back to local if necessary -->
+  	<script src="js/jquery.min.js"></script>
+  	<script>!window.jQuery && document.write(unescape('%3Cscript src="js/libs/jquery-1.4.2.min.js"%3E%3C/script%3E'))</script>
+  	<!-- scripts concatenated and minified via ant build script-->
+  	<script src="js/plugins.js"></script>
+  	<script src="js/script.js"></script>
+  	<!-- end concatenated and minified scripts-->
+
+	<!--[if lt IE 7 ]>
+		<script src="js/libs/dd_belatedpng.js"></script>
+		<script> DD_belatedPNG.fix('img, .png_bg, .sidebar, .sidebar ul, .sidebar ul li, .sidebar ul li a'); //fix any <img> or .png_bg background-images </script>
+	<![endif]-->
+</body>
+</html>
