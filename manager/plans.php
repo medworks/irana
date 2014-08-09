fa3f4b3e4778206241965941c8212f89c27fbe77
Saeid Hatami <hatami4510@gmail.com>
2014-08-09 19:25:38 +0430

    added php codes ...

--- manager/plans.php
+++ manager/plans.php
@@ -3,7 +3,49 @@
     
 	//ob_start(); 	
 	//$table = include_once("inc/table.php");
-	//ob_end_clean();	
+	//ob_end_clean();
+
+if ($_POST["mark"]=="saveplan")
+	{						   				
+		$fields = array("`pname`","`month`","`gig`","`night`","`modem`","`price`","`percent`");		
+		$values = array("'{$_POST[plan]}'","'{$_POST[month]}'","'{$_POST[volume]}'",
+						"'{$_POST[night]}'","'{$_POST[modem]}'","'{$_POST[price]}'","'{$_POST[percent]}'");	
+		if (!$db->InsertQuery('plans',$fields,$values)) 
+		{			
+			header('location:plans.html?msg=2');
+		} 	
+		else 
+		{  										
+			header('location:plans.html?msg=1');									
+		}  				 
+	}
+	else
+	if ($_POST["mark"]=="editplan")
+	{		
+	    $_POST["detail"] = addslashes($_POST["detail"]);
+		$values = array("`pname`"=>"'{$_POST[plan]}'",
+		                 "`month`"=>"'{$_POST[month]}'",
+						 "`gig`"=>"'{$_POST[volume]}'",
+						 "`night`"=>"'{$_POST[night]}'",
+						 "`modem`"=>"'{$_POST[modem]}'",
+						 "`price`"=>"'{$_POST[price]}'",
+						 "`percent`"=>"'{$_POST[percent]}'");
+        $db->UpdateQuery("plans",$values,array("id='{$_GET[pid]}'"));		
+		header('location:plans.html?msg=2');
+	}	
+	
+	if ($_GET['act']=="new")
+	{
+		$insertoredit = "
+			<p><input type='submit' style='width:70px;height:35px' value='ثبت'/></p>
+						<input type='hidden' name='mark' value='saveplan' />  ";
+	}
+	if ($_GET['act']=="edit")
+	{
+		$insertoredit = "
+			<p><input type='submit' style='width:70px;height:35px' value='ویرایش'/></p>
+						<input type='hidden' name='mark' value='saveplan' />  ";
+	}
 
 $html =<<<cd
 	<!-- Main Section -->
@@ -24,8 +66,7 @@ $html =<<<cd
 						<p style="padding-top:10px"><span>مودم دارد</span><input type="checkbox" name="modem" value="" /></p>
 						<p class="clear"></p>
                         <p><span>درصد تخفیف</span><input type="text" name="percent" placeholder="1-100" /></p>
-						<p><input type="submit" style="width:70px;height:35px" value="ثبت"/></p>
-						<input type="hidden" name="mark" value="plan" />  
+						{$insertoredit}						
 					</form>
                     <div class="clear"></div>
                     <hr>
