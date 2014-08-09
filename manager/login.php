fe28d81a40d3adaf43c6a35eb2a727d99eac4d36
Saeid Hatami <hatami4510@gmail.com>
2014-08-09 18:45:37 +0430

    add php codes of login page

--- manager/login.php
+++ manager/login.php
@@ -1,9 +1,36 @@
 <?php
-    header('Content-Type: text/html; charset=UTF-8');
-?>
+ //header('Content-Type: text/html; charset=UTF-8');
+include_once("../config.php");
+include_once("../classes/database.php");
+include_once("../classes/session.php");
+include_once("../classes/login.php");
+include_once("../classes/messages.php");
+ 
+$login=Login::GetLogin();
+$msg=Message::GetMessage();
+$msg = "";
+if ($login->IsLogged())
+{	
+		header("Location: ../manager/admin.php");
+} 
+else
+{
+	if (isset ($_POST["mark"]) AND $_POST["mark"] == "adminlogin")
+	{
+		if ($login->AdminLogin($_POST['username'],$_POST['password']))
+		{		 
+			header("location:admin.php");			
+		}	
+		else
+		{ 
+			$msg = $msg->ShowError("نام کاربری یا کلمه عبور اشتباه می باشد !");			
+		}	
+	}   
+
+$html=<<<cd
 <!DOCTYPE html>
 <html lang="fa"><head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Irana | Login page</title>
 
 <link rel="stylesheet" media="screen" href="css/reset.css">
@@ -54,7 +81,7 @@ $(document).ready(function(){
       <header><h2>ورود به پنل مدیرت شرکت ایرانا</h2></header>
     	<section>
     		<div class="message info"><p style="margin:0;font-size:15px;">نام کاربری و رمز عبور خود را وارد نمایید</p></div>
-    		<form id="form" action="dashboard.html" method="post" class="clearfix" novalidate="novalidate">
+    		<form id="form" action="" method="post" class="clearfix" novalidate="novalidate">
     			<p>
     				<input type="text" id="username" class="full" value="" name="username" required="required" placeholder="نام کاربری">
     			</p>
@@ -64,11 +91,20 @@ $(document).ready(function(){
     			<p class="clearfix">
     				<button class="button button-gray fr" type="submit">ورود</button>
     			</p>
+				<input type="hidden" name="mark" value="adminlogin" />    
     		</form>
     	</section>
     </div>
     <div class="apple_overlay black" id="overlay">
         <iframe class="contentWrap" style="width: 100%; height: 500px"></iframe>
     </div>
+	<div class="message">
+				{$msg}
+    </div>
 </body>
 </html>
+cd;
+
+echo $html;
+}
+?>
