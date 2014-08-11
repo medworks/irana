09ede631940c0d9dd9212b9cea9d782019577c59
Saeid Hatami <hatami4510@gmail.com>
2014-08-12 01:00:21 +0430

    compelete setting page codes

--- manager/setting.php
+++ manager/setting.php
@@ -5,7 +5,6 @@
 	include_once("../classes/messages.php");
 	include_once("../classes/session.php");	
 	include_once("../classes/functions.php");
-	include_once("../lib/persiandate.php");	
 	include_once("../classes/login.php");
 	
 	$login = Login::GetLogin();
@@ -15,8 +14,7 @@
 		die(); // solve a security bug
 	}
 	
-	$db = Database::GetDatabase();
-	
+		
 	$About_System = GetSettingValue('About_System',0);
 	$Site_Title = GetSettingValue('Site_Title',0);
 	$Site_KeyWords = GetSettingValue('Site_KeyWords',0);
@@ -26,16 +24,17 @@
 	$Fax_Number = GetSettingValue('Fax_Number',0);
 	$Address = GetSettingValue('Address',0);
 	
-	if ($_POST["editsetting"])
+	if ($_POST["mark"]==editsetting)
 	{
-		SetSettingValue("About_System",$_POST["about"]);
+		SetSettingValue("About_System",$_POST["about"]);		
 		SetSettingValue("Site_Title",$_POST["title"]);
 		SetSettingValue("Site_KeyWords",$_POST["keywords"]);
 		SetSettingValue("Site_Describtion",$_POST["detail"]);
 		SetSettingValue("Contact_Email",$_POST["email"]);
 		SetSettingValue("Tell_Number",$_POST["tel"]);
 		SetSettingValue("Fax_Number",$_POST["fax"]);
-		SetSettingValue("Address",$_POST["Address"]);
+		SetSettingValue("Address",$_POST["address"]);
+		header('location:setting.php');
 	}
 $html=<<<cd
 	<!-- Main Section -->
@@ -56,7 +55,7 @@ $html=<<<cd
 						<p><span>ایمیل</span><input type="text" name="email" class="ltr" style="width:250px" placeholder="name@domain.com" value="{$Contact_Email}" /></p>
 						<p><span>تلفن</span><input type="text" name="tel" class="ltr" placeholder="51-38555560" value="{$Tell_Number}" /></p>
                         <p><span>فاکس</span><input type="text" name="fax" class="ltr" placeholder="51-38555560" value="{$Fax_Number}" /></p>
-                        <p><span style="display:block;margin-bottom:5px;">آدرس</span><textarea name="address">{{$Address}}</textarea></p>
+                        <p><span style="display:block;margin-bottom:5px;">آدرس</span><textarea name="address">{$Address}</textarea></p>
 						<p><input type="submit" style="width:70px;height:35px" value="ثبت"/></p>
 						<input type='hidden' name='mark' value='editsetting' />
 					</form>
