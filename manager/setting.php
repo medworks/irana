e6b20a54624effc981feed2e167a0b154d8d7bcf
Saeid Hatami <hatami4510@gmail.com>
2014-08-12 00:34:17 +0430

    add saving codes

--- manager/setting.php
+++ manager/setting.php
@@ -15,6 +15,17 @@
 		die(); // solve a security bug
 	 }
 	$db = Database::GetDatabase();
+	if ($_POST["editsetting"])
+	{
+		SetSettingValue("About_System",$_POST["about"]);
+		SetSettingValue("Site_Title",$_POST["title"]);
+		SetSettingValue("Site_KeyWords",$_POST["keywords"]);
+		SetSettingValue("Site_Describtion",$_POST["detail"]);
+		SetSettingValue("Contact_Email",$_POST["email"]);
+		SetSettingValue("Tell_Number",$_POST["tel"]);
+		SetSettingValue("Fax_Number",$_POST["fax"]);
+		SetSettingValue("Address",$_POST["Address"]);
+	}
 $html=<<<cd
 	<!-- Main Section -->
     <section class="main-section grid_7">
@@ -27,14 +38,14 @@ $html=<<<cd
             <section class="container_6 clearfix">
                 <div class="grid_6">
 					<form class="setting" action="" method="post">
-						<p><span style="display:block;margin-bottom:5px;">درباره ما</span><textarea>درباره ما</textarea></p>
+						<p><span style="display:block;margin-bottom:5px;">درباره ما</span><textarea name="about">درباره ما</textarea></p>
                         <p><span>عنوان سایت</span><input type="text" name="title" placeholder="عنوان سایت"/></p>
                         <p><span>کلمات کلیدی</span><input type="text" name="keywords" style="width:450px" placeholder="کلمات کلیدی"/></p>
                         <p><span>توضیحات سایت در موتورهای جستجو</span><input type="text" name="detail" style="width:500px" placeholder="توضیحات سایت در موتورهای جستجو"/></p>
 						<p><span>ایمیل</span><input type="text" name="email" class="ltr" style="width:250px" placeholder="name@domain.com"/></p>
-						<p><span>تلفن (گیگابایت)</span><input type="text" name="tel" class="ltr" placeholder="51-38555560"/></p>
+						<p><span>تلفن</span><input type="text" name="tel" class="ltr" placeholder="51-38555560"/></p>
                         <p><span>فاکس</span><input type="text" name="fax" class="ltr" placeholder="51-38555560" /></p>
-                        <p><span style="display:block;margin-bottom:5px;">آدرس</span><textarea>آدرس</textarea></p>
+                        <p><span style="display:block;margin-bottom:5px;">آدرس</span><textarea name="address">آدرس</textarea></p>
 						<p><input type="submit" style="width:70px;height:35px" value="ثبت"/></p>
 						<input type='hidden' name='mark' value='editsetting' />
 					</form>
