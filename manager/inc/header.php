096af7f7a2518b87c9a34c491f3015d5572ef890
Saeid Hatami <hatami4510@gmail.com>
2014-08-12 01:04:50 +0430

    add logout link

--- manager/inc/header.php
+++ manager/inc/header.php
@@ -48,7 +48,7 @@
                             <ul>
                                 <li><a href="setting.php">تنظیمات</a></li>
                                 <li><a href="#">تعریف کاربر</a></li>
-                                <li><a href="#">خارج شدن</a></li>
+                                <li><a href="admin.php?act=logout">خارج شدن</a></li>
                             </ul>
                         </li>
                     </ul>
