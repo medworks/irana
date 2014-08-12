4dd23cc560122acad872b5113c859296c361a95e
Saeid Hatami <hatami4510@gmail.com>
2014-08-12 10:32:47 +0430

    add logout codes

--- manager/admin.php
+++ manager/admin.php
@@ -1,5 +1,29 @@
 <?php
     include_once("inc/header.php");
+	include_once("../config.php");
+	include_once("../classes/functions.php");
+  	include_once("../classes/messages.php");
+  	include_once("../classes/session.php");	
+  	include_once("../classes/security.php");
+  	include_once("../classes/database.php");	
+	include_once("../classes/login.php");
+    include_once("../lib/persiandate.php");  	
+	
+	$login = Login::GetLogin();	
+	if (!$login->IsLogged())
+	{
+		header("Location: ../index.php");
+		die(); //solve security bug
+	}	
+	$mes = Message::GetMessage();
+	if ($_GET["act"] == "logout")
+   {
+	   if ($login->LogOut())
+			header("Location: ../index.php");
+	   else
+		    echo $mes->ShowError("عملیات خروج با خطا مواجه شد، لطفا مجددا سعی نمایید.");
+   }
+	
 ?>
     
     <!-- Main Section -->
