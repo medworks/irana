a8f017a3ac00477c479e0bb195ec353d5e188a24
mojiAm <amjadi.mojtaba@gmail.com>
2014-08-21 20:50:11 +0330

    fix ltr

--- manager/sms.php
+++ manager/sms.php
@@ -17,12 +17,15 @@
 	
 	$SmsUserName = GetSettingValue('SmsUserName',0);
 	$SmsPassWord = GetSettingValue('SmsPassWord',0);
+	$SmsLineNumber = GetSettingValue('SmsLineNumber',0);
 	$SmsText = GetSettingValue('SmsText',0);
+	
 	
     if ($_POST["mark"]==editsetting)
 	{
 	    SetSettingValue("SmsUserName",$_POST["smsuser"]);		
-		SetSettingValue("SmsPassWord",$_POST["smspassword"]);		
+		SetSettingValue("SmsPassWord",$_POST["smspassword"]);
+		SetSettingValue("SmsLineNumber",$_POST["smslinenumber"]);
 		SetSettingValue("SmsText",$_POST["smstext"]);
 		header('location:sms.php');
 	}
@@ -38,8 +41,9 @@ $html=<<<cd
             <section class="container_6 clearfix">
                 <div class="grid_6">
 					<form class="setting" action="" method="post">
-					    <p><span style="display:block;margin-bottom:5px;">نام کاربری اس ام اس</span><input type="text" name="smsuser" placeholder="نام کاربری" value="{$SmsUserName}"  /></p>
-					    <p><span style="display:block;margin-bottom:5px;">رمز عبور اس ام اس</span><input type="password" name="smspassword" placeholder="رمز عبور" value="{$SmsPassWord}" /></p>					    
+					    <p><span style="display:block;margin-bottom:5px;">نام کاربری اس ام اس</span><input type="text" name="smsuser" class="ltr" placeholder="نام کاربری" value="{$SmsUserName}"  /></p>
+					    <p><span style="display:block;margin-bottom:5px;">رمز عبور اس ام اس</span><input type="password" name="smspassword" class="ltr" placeholder="رمز عبور" value="{$SmsPassWord}" /></p>
+						<p><span style="display:block;margin-bottom:5px;">شماره خط اسم ام اس</span><input type="text" name="smslinenumber" class="ltr" placeholder="شماره خط" value="{$SmsLineNumber}"  /></p>
 						<p><span style="display:block;margin-bottom:5px;">متن پیام اس ام اس</span><textarea name="smstext">{$SmsText}</textarea></p>
                         <p><input type="submit" style="width:70px;height:35px" value="ثبت"/></p>
 						<input type='hidden' name='mark' value='editsetting' />
