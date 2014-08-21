626fb0460aa67622dff2e070e61c26705f0749d9
Saeid Hatami <hatami4510@gmail.com>
2014-08-21 20:35:13 +0430

    fix errors

--- manager/sms.php
+++ manager/sms.php
@@ -17,12 +17,15 @@
 	
 	$SmsUserName = GetSettingValue('SmsUserName',0);
 	$SmsPassWord = GetSettingValue('SmsPassWord',0);
+	$SmsLineNumber = GetSettingValue('SmsLineNumber',0);
 	$SmsText = GetSettingValue('SmsText',0);
+	
 	
     if ($_POST["mark"]==editsetting)
 	{
 	    SetSettingValue("SmsUserName",$_POST["smsuser"]);		
-		SetSettingValue("SmsPassWord",$_POST["smspassword"]);		
+		SetSettingValue("SmsPassWord",$_POST["smspassword"]);
+		SetSettingValue("SmsLineNumber",$_POST["smslinenumber"]);
 		SetSettingValue("SmsText",$_POST["smstext"]);
 		header('location:sms.php');
 	}
@@ -39,7 +42,8 @@ $html=<<<cd
                 <div class="grid_6">
 					<form class="setting" action="" method="post">
 					    <p><span style="display:block;margin-bottom:5px;">نام کاربری اس ام اس</span><input type="text" name="smsuser" placeholder="نام کاربری" value="{$SmsUserName}"  /></p>
-					    <p><span style="display:block;margin-bottom:5px;">رمز عبور اس ام اس</span><input type="password" name="smspassword" placeholder="رمز عبور" value="{$SmsPassWord}" /></p>					    
+					    <p><span style="display:block;margin-bottom:5px;">رمز عبور اس ام اس</span><input type="password" name="smspassword" placeholder="رمز عبور" value="{$SmsPassWord}" /></p>
+						<p><span style="display:block;margin-bottom:5px;">شماره خط اسم ام اس</span><input type="text" name="smslinenumber" placeholder="شماره خط" value="{$SmsLineNumber}"  /></p>
 						<p><span style="display:block;margin-bottom:5px;">متن پیام اس ام اس</span><textarea name="smstext">{$SmsText}</textarea></p>
                         <p><input type="submit" style="width:70px;height:35px" value="ثبت"/></p>
 						<input type='hidden' name='mark' value='editsetting' />
