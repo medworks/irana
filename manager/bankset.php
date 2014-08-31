ffaeae583fa168fe5783fbb93a38efb657cb94dd
Saeid Hatami <hatami4510@gmail.com>
2014-08-31 23:57:55 +0430

    fix errors

--- manager/bankset.php
+++ manager/bankset.php
@@ -40,9 +40,9 @@ $html=<<<cd
             <section class="container_6 clearfix">
                 <div class="grid_6">
 					<form class="setting" action="" method="post">
-					    <p><span style="display:block;margin-bottom:5px;">ترمینال</span><input type="text" name="Bank_Terminal_ID" class="ltr" /></p>
-					    <p><span style="display:block;margin-bottom:5px;">نام کاربری</span><input type="text" name="Bank_User_Name" class="ltr" /></p>
-						<p><span style="display:block;margin-bottom:5px;">رمز عبور</span><input type="password" name="Bank_Pass_Word" class="ltr" /></p>
+					    <p><span style="display:block;margin-bottom:5px;">ترمینال</span><input type="text" name="Bank_Terminal_ID" class="ltr" value="{$Bank_Terminal_ID}" /></p>
+					    <p><span style="display:block;margin-bottom:5px;">نام کاربری</span><input type="text" name="Bank_User_Name" class="ltr" value="{$Bank_User_Name}" /></p>
+						<p><span style="display:block;margin-bottom:5px;">رمز عبور</span><input type="password" name="Bank_Pass_Word" class="ltr" value="{$Bank_Pass_Word}" /></p>
 						<p><input type="submit" style="width:70px;height:35px" value="ثبت"/></p>
 						<input type='hidden' name='mark' value='editsetting' />
 					</form>
