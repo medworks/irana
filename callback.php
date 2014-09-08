a3c507ad6dda9bad7f6ebdbc4c7bb4eddc1c49ec
Saeid Hatami <hatami4510@gmail.com>
2014-09-08 17:05:33 +0430

    fix and ADD NEW DATABASE

--- callback.php
+++ callback.php
@@ -88,6 +88,7 @@ if ($_POST['ResCode'] == "17") // when user click on cancel paying payment page
 			$resultStr = $array["return"];
 			$ressettle = explode (',',$resultStr);
 			$ResCodesettle = $ressettle[0];
+			$ResCode = $ressettle[0];
 			if ($ResCodesettle == "0" Or $ResCodesettle=="45") 
 			{
 				$paymentdone=1;
@@ -152,7 +153,7 @@ cd;
 					"`pegiri`"=>"'{$_POST['SaleReferenceId']}'",
 	                "`selorder`"=>"'{$_POST['SaleOrderId']}'",
 					"`regdate`"=>"'{$date}'",
-					"`errcode`"=>"'{$_POST['ResCode']}'",
+					"`errcode`"=>"'{$_POST['ResCode']} - {$ResCode}'",
 					"`confirm`"=>"'{$paymentdone}'");
     //$id = $db->MaxOfAll("id", "payment");
 	$id = $sess->Get("payment_id");
