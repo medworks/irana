3e65fe70ae53acc13d57c92ff0c44d0deb315514
Saeid Hatami <hatami4510@gmail.com>
2014-09-29 22:05:46 +0330

    add order sms sending ability to admin mobile

--- manager/setting.php
+++ manager/setting.php
@@ -14,17 +14,6 @@
 		die(); // solve a security bug
 	}
 	
-    $Extra_Tax = GetSettingValue('Extra_Tax',0);
-	$Email_Text = GetSettingValue('Email_Text',0);
-	$About_System = GetSettingValue('About_System',0);
-	$Site_Title = GetSettingValue('Site_Title',0);
-	$Site_KeyWords = GetSettingValue('Site_KeyWords',0);
-	$Site_Describtion = GetSettingValue('Site_Describtion',0);
-	$Contact_Email = GetSettingValue('Contact_Email',0);
-	$Tell_Number = GetSettingValue('Tell_Number',0);
-	$Fax_Number = GetSettingValue('Fax_Number',0);
-	$Address = GetSettingValue('Address',0);
-	
 	if ($_POST["mark"]==editsetting)
 	{
 	    SetSettingValue("Extra_Tax",$_POST["extratax"]);
@@ -37,8 +26,27 @@
 		SetSettingValue("Tell_Number",$_POST["tel"]);
 		SetSettingValue("Fax_Number",$_POST["fax"]);
 		SetSettingValue("Address",$_POST["address"]);
+		SetSettingValue("Is_Send_Order_Sms_For_Admin",$_POST["Is_Send_Order_Sms_For_Admin"]);
+		SetSettingValue("Admin_Mobile_Number",$_POST["Admin_Mobile_Number"]);
+				
 		header('location:setting.php');
 	}
+	
+    $Extra_Tax = GetSettingValue('Extra_Tax',0);
+	$Email_Text = GetSettingValue('Email_Text',0);
+	$About_System = GetSettingValue('About_System',0);
+	$Site_Title = GetSettingValue('Site_Title',0);
+	$Site_KeyWords = GetSettingValue('Site_KeyWords',0);
+	$Site_Describtion = GetSettingValue('Site_Describtion',0);
+	$Contact_Email = GetSettingValue('Contact_Email',0);
+	$Tell_Number = GetSettingValue('Tell_Number',0);
+	$Fax_Number = GetSettingValue('Fax_Number',0);
+	$Address = GetSettingValue('Address',0);	
+	$Is_Send_Order_Sms_For_Admin = GetSettingValue('Is_Send_Order_Sms_For_Admin',0);
+	$Admin_Mobile_Number = GetSettingValue('Admin_Mobile_Number',0);
+	$Is_Send_Order_Sms_For_Admin_Ok=($Is_Send_Order_Sms_For_Admin==1)?"checked":"";
+	
+	
 $html=<<<cd
 	<!-- Main Section -->
     <section class="main-section grid_7">
@@ -51,6 +59,8 @@ $html=<<<cd
             <section class="container_6 clearfix">
                 <div class="grid_6">
 					<form class="setting" action="" method="post">
+					     <p style="padding-top:10px"><span>ارسال اس ام اس سفارش برای مدیریت</span><input type="checkbox" name="Is_Send_Order_Sms_For_Admin" value="1" {$Is_Send_Order_Sms_For_Admin_Ok}/></p>
+					     <p><span style="display:block;margin-bottom:5px;"> شماره موبایل مدیریت جهت دریافت اس ام اس سفارش</span><input class="ltr" type="text" name="Admin_Mobile_Number" placeholder="091 ..." value="{$Admin_Mobile_Number}" /></p>
 					    <p><span style="display:block;margin-bottom:5px;">مالیات بر ارزش افزوده (درصد)</span><input type="text" name="extratax" placeholder="1-100" value="{$Extra_Tax}" /></p>
 						<p><span style="display:block;margin-bottom:5px;">متن تایدیه ایمیل </span><textarea name="emailtext">{$Email_Text}</textarea></p>
 						<p><span style="display:block;margin-bottom:5px;">درباره ما</span><textarea name="about">{$About_System}</textarea></p>
