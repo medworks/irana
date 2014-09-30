b6ef65053d1145d06c9aee983610d05e92709aa2
mojiAm <amjadi.mojtaba@gmail.com>
2014-09-30 11:34:18 +0330

    add icon

--- price.php
+++ price.php
@@ -1,67 +1,67 @@
 <?php
-	include_once("inc/header.php");
-	
-	include_once("config.php");
+  include_once("inc/header.php");
+  
+  include_once("config.php");
     include_once("classes/database.php");
     include_once("classes/messages.php");
-	include_once("classes/functions.php");
-	include_once("/lib/persiandate.php");
+  include_once("classes/functions.php");
+  include_once("/lib/persiandate.php");
   
     $db = Database::GetDatabase();
-	$msg = Message::GetMessage();
+  $msg = Message::GetMessage();
 $html =<<<cd
-	<link rel="stylesheet" href="manager/css/tables.css" type="text/css">
-	<link rel="stylesheet" href="manager/css/forms.css" type="text/css">
+  <link rel="stylesheet" href="manager/css/tables.css" type="text/css">
+  <link rel="stylesheet" href="manager/css/forms.css" type="text/css">
 
-  	<!-- <script src="manager/js/jquery.tools.min.js"></script>
-  	<script src="manager/js/jquery.ui.min.js"></script>
-  	<script src="manager/js/jquery.tables.js"></script> -->
-  	<!-- <style>
-  		a.button-blue{
-  			background: -webkit-gradient(linear, left top, left bottom, from(#faa51a), to(#f47a20)) !important;
-  			color:#2f2f2f !important;
-  			font-size:15px !important;
-  			border:1px solid #dd6611 !important;
-  		}
+    <!-- <script src="manager/js/jquery.tools.min.js"></script>
+    <script src="manager/js/jquery.ui.min.js"></script>
+    <script src="manager/js/jquery.tables.js"></script> -->
+    <!-- <style>
+      a.button-blue{
+        background: -webkit-gradient(linear, left top, left bottom, from(#faa51a), to(#f47a20)) !important;
+        color:#2f2f2f !important;
+        font-size:15px !important;
+        border:1px solid #dd6611 !important;
+      }
 
-  		a.button-blue:hover,
-  		a.button-blue:focus{
-  			background: -webkit-gradient(linear, left top, left bottom, from(#f88e11), to(#f06015)) !important;
-  			border: solid 1px #aa5511 !important;
-  			color: #fef4e9 !important;
-  		}
+      a.button-blue:hover,
+      a.button-blue:focus{
+        background: -webkit-gradient(linear, left top, left bottom, from(#f88e11), to(#f06015)) !important;
+        border: solid 1px #aa5511 !important;
+        color: #fef4e9 !important;
+      }
 
-  		a.button-blue.current{
-  			color:#fff !important;
-  		}
-  	</style> -->
+      a.button-blue.current{
+        color:#fff !important;
+      }
+    </style> -->
 
-		<!-- Main content alpha -->
-		<div class="main png_bg">
-			<div class="inner_main">
-			<!-- True containers (keep the content inside containers!) -->
-				<div class="container_alpha slogan">
-					<h1>تعرفه طرح ها</h1>
-				</div>
-				<div class="container_gamma slogan" style="background:none">
+    <!-- Main content alpha -->
+    <div class="main png_bg">
+      <div class="inner_main">
+      <!-- True containers (keep the content inside containers!) -->
+        <div class="container_alpha slogan">
+          <h1>تعرفه طرح ها</h1>
+        </div>
+        <div class="container_gamma slogan" style="background:none">
 cd;
-$rows = $db->SelectAll("plans","*",null,"id Asc");
+$rows = $db->SelectAll("plans","*"," remove = 0 ","position Asc");
 $table=<<<cd
 <table class="datatable full">
     <thead class="rtl">
-        <tr>	        
+        <tr>          
           <th>نام طرح</th>
           <th>مدت زمان (ماه)</th>
-          <th>حجم (گیگابایت)</th>
+          <!-- <th>حجم (گیگابایت)</th> -->
           <th>شبانه رایگان</th>
           <th>مودم رایگان</th>
           <th>هزینه طرح (ریال)</th>
           <th>درصد تخفیف</th>
           <th>قابل پرداخت (ریال)</th>   
-		      <th>سفارش</th>		
+          <th>سفارش</th>    
         </tr>
     </thead>
-	<tbody class="rtl">
+  <tbody class="rtl">
 cd;
 for($i = 0; $i < Count($rows); $i++)
 {
@@ -72,9 +72,9 @@ for($i = 0; $i < Count($rows); $i++)
  $percent = $rows[$i]["percent"]." % ";
 
 if (($i+1)%10 == 0)
-	
-	$table.=<<<cd
-	</tbody>
+  
+  $table.=<<<cd
+  </tbody>
 <tbody class="rtl" style="display: table-row-group;">
 cd;
 
@@ -88,47 +88,47 @@ else
    $class = "";
  }
 $table .=<<<cd
-        <tr class="$class">		
+        <tr class="$class">   
             <td>{$rows[$i]["pname"]}</td>
             <td>{$rows[$i]["month"]}</td>
-            <td>{$rows[$i]["gig"]}</td>
+            <!-- <td>{$rows[$i]["gig"]}</td> -->
             <td>{$rows[$i]["night"]}</td>
             <td>{$rows[$i]["modem"]}</td>
-      			<td>{$rows[$i]["price"]}</td>			
-      			<td>{$percent}</td>			
+            <td>{$rows[$i]["price"]}</td>     
+            <td>{$percent}</td>     
             <td>{$percentedprice}</td>
-      			<td>
+            <td>
               <a href="main.php?act=neword&planid={$rows[$i]["id"]}" class="button button-gray no-text" style="display:inline-block;padding:0 10px;">
                 <span class="order"></span>
               </a>
             </td>
         </tr>
-	
+  
 cd;
 }
 $table.="</tbody> </table>";
 $html.=<<<cd
-					{$table}
-					<div class="clearfix"></div>
-				</div>
-				<div class="container_alpha slogan">
-					<h1>تعرفه حجم ها</h1>
-				</div>
-				<div class="container_gamma slogan" style="background:none">
+          {$table}
+          <div class="clearfix"></div>
+        </div>
+        <div class="container_alpha slogan">
+          <h1>تعرفه حجم ها</h1>
+        </div>
+        <div class="container_gamma slogan" style="background:none">
 cd;
 $rows = $db->SelectAll("volumes","*",null,"id Asc");
 $table=<<<cd
 <table class="datatable full">
     <thead class="rtl">
-        <tr>	        
+        <tr>          
             <th>از حجم (گیگابایت)</th>
             <th>تا حجم (گیگابایت)</th>
             <th>قیمت (ریال)</th>
             <th>درصد تخفیف</th>                 
-            <th>قابل پرداخت (ریال)</th>            			
+            <th>قابل پرداخت (ریال)</th>                 
         </tr>
     </thead>
-	<tbody class="rtl">
+  <tbody class="rtl">
 cd;
 for($i = 0; $i < Count($rows); $i++)
 {
@@ -137,35 +137,35 @@ for($i = 0; $i < Count($rows); $i++)
  $rows[$i]["percent"] = $rows[$i]["percent"]+" % ";
  $percentedprice = $rows[$i]["price"]-(($rows[$i]["price"]*$rows[$i]["percent"])/100);
 if (($i+1)%10 == 0)
-	
-	$table.=<<<cd
-	</tbody>
+  
+  $table.=<<<cd
+  </tbody>
 <tbody style="display: table-row-group;">
 cd;
 
 $table .=<<<cd
-        <tr>		
+        <tr>    
             <td>{$rows[$i]["fvol"]}</td>
             <td>{$rows[$i]["tvol"]}</td>
             <td>{$rows[$i]["price"]}</td>
             <td>{$percent}</td>
-			<td>{$percentedprice}</td>
+      <td>{$percentedprice}</td>
         </tr>
-	
+  
 cd;
 }
 $table.="</tbody> </table>";
 $html.=<<<cd
-					{$table}
-					<div class="clearfix"> </div>
-				</div>
-			</div>
-		</div>
-		<!-- /True containers (keep the content inside containers!) -->
-    	<div class="endmain png_bg"></div>
-		<!-- /Main content alpha -->
-	
+          {$table}
+          <div class="clearfix"> </div>
+        </div>
+      </div>
+    </div>
+    <!-- /True containers (keep the content inside containers!) -->
+      <div class="endmain png_bg"></div>
+    <!-- /Main content alpha -->
+  
 cd;
     echo $html;
-	include_once("inc/footer.php");
+  include_once("inc/footer.php");
 ?>
